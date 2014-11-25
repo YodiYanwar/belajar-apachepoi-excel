ac60ae9f5c94c1e54434329627be22984522058a
YodiYanwar <yodieys_yanwar@windowslive.com>
2014-11-25 10:08:36 +0700

    First Commit

---
+++ src/main/java/com/yodiyanwar/App.java
@@ -0,0 +1,15 @@
+package com.yodiyanwar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Operasi operasi = new Operasi();
+        operasi.buatBaru();
+        operasi.bacaFile();
+    }
+}
